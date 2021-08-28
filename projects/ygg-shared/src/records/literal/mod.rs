637667344232ce09a1049a4d0b121987a7d1419d
Aster <galaster@foxmail.com>
2021-08-28 14:46:34 +0800

    🚀 Add literal type

---
+++ projects/ygg-shared/src/records/literal/mod.rs
@@ -0,0 +1,17 @@
+mod traits;
+
+use std::{cmp::Ordering, ops::Deref};
+use std::ops::Range;
+
+/// Maybe have ast position
+pub type MaybeRanged = Option<Range<usize>>;
+
+/// Used to represent a node with positions
+#[derive(Clone)]
+pub struct Literal<T> {
+    /// The actual value
+    pub value: T,
+    /// The Start offset and end offset
+    pub range: MaybeRanged,
+}
+
